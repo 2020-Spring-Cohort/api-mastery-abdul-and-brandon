d0352bab9bb1aa945760ebdca6d849972f8fbe9f
brandonroth02 <59844743+brandonroth02@users.noreply.github.com>
2020-03-18 20:16:45 -0400

    working on director controller

--- backend-api/src/main/java/org/wcci/apimastery/Controllers/DirectorController.java
+++ backend-api/src/main/java/org/wcci/apimastery/Controllers/DirectorController.java
@@ -1,4 +1,26 @@
 package org.wcci.apimastery.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.wcci.apimastery.Models.Director;
+import org.wcci.apimastery.Storage.ArtistStorage;
+import org.wcci.apimastery.Storage.DirectorStorage;
+import org.wcci.apimastery.models.Artist;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/directors")
+
 public class DirectorController {
+
+    private DirectorStorage directorStorage;
+
+    public DirectorController(DirectorStorage directorStorage) { this.directorStorage = directorStorage;}
+
+    @GetMapping
+    public Collection<Director>
 }
