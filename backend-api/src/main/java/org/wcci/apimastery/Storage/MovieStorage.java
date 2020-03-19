02308380e19d6f203598a88d3074e98d12c83fdf
brandonroth02 <59844743+brandonroth02@users.noreply.github.com>
2020-03-19 12:48:55 -0400

    completed MovieStorage

--- backend-api/src/main/java/org/wcci/apimastery/Storage/MovieStorage.java
+++ backend-api/src/main/java/org/wcci/apimastery/Storage/MovieStorage.java
@@ -1,4 +1,26 @@
 package org.wcci.apimastery.Storage;
 
+import org.wcci.apimastery.Models.Genre;
+import org.wcci.apimastery.Models.Movie;
+import org.wcci.apimastery.Models.Director;
+
+import java.util.Optional;
+
 public interface MovieStorage {
+
+
+    Optional<Movie> findSongByTitle(String title);
+
+    Iterable<Movie> findAllByDirector(Director director);
+
+    Iterable<Movie> findAllByArtistName(String name);
+
+
+    Iterable<Movie> findAllByAlbum(Genre genre);
+
+    Iterable<Movie> findAllyByAlbumTitle(String title);
+
+    Iterable<Movie> findAllMovies();
+
+
 }
